e92977506346f44d4a91142df4f6e4d277cd73fa
rmurray <rmurray@safaricom.co.ke>
2019-05-13 17:48:02 +0300

    adding new files

---
+++ src/main/java/com/csm/rabbithole/config/RabbitMQConfig.java
@@ -0,0 +1,49 @@
+package com.csm.rabbithole.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${rabbithole.rabbitmq.queue}")
+    String queueName;
+
+    @Value("${rabbithole.rabbitmq.exchange}")
+    String exchangeName;
+
+    @Value("${rabbithole.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return  rabbitTemplate;
+    }
+}
